3610bb63627750b6cf3e0748bf319e9304b90e32
haidi20 <nhaidii@yahoo.com>
2018-03-20 10:15:53 +0800

    modal notif register

--- src/components/auth/registerasi.vue
+++ src/components/auth/registerasi.vue
@@ -50,7 +50,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title">Modal title</h4>
+            <h4 class="modal-title">Pemberitahuan</h4>
           </div>
           <div class="modal-body">
             <!-- <p v-if="notif.length">{{notif}}</p>
